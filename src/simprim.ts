464612c7c2f31781638d55355135944b61925a0b
Nfolio <139557367+Nfolio567@users.noreply.github.com>
2025-05-23 22:23:38 +0900

    1.1.beta

--- src/simprim.ts
+++ src/simprim.ts
@@ -238,7 +238,6 @@ class SimPrim {
         function funcResizing(this: SimPrim, e: MouseEvent) {
             // Trimming area resizing process
             if (this.resizing && this.dx !== undefined && this.dy !== undefined) {
-                console.log(property);
                 if (this.drawTrimmingWidth <= 0 || this.drawTrimmingHeight <= 0) {
                     this.drawTrimmingHeight = 0;
                     this.drawTrimmingWidth = this.drawTrimmingHeight;
@@ -249,6 +248,7 @@ class SimPrim {
                 beforeWidth = this.drawTrimmingWidth;
                 beforeHeight = this.drawTrimmingHeight;
                 if (property == "downR" && this.img) {
+                    console.log(property);
                     this.inputCvs.style.cursor = "nwse-resize";
 
                     // Resize detection
