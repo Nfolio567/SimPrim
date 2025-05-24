3fb92554c42c6329ebdbb5c689c54b642df73dfb
Nfolio <139557367+Nfolio567@users.noreply.github.com>
2025-05-24 17:34:35 +0900

    1.1.beta

--- src/simprim.ts
+++ src/simprim.ts
@@ -313,14 +313,14 @@ class SimPrim {
                         this.drawTrimmingWidth -= e.movementX * this.scaleWidth / zoomClearance;
                     }
                     if (e.movementY != 0 && e.movementX == 0) {
-                        this.dx -= e.movementY * this.scaleHeight / zoomClearance;
-                        this.drawTrimmingWidth += e.movementY * this.scaleHeight / zoomClearance;
+                        this.dx += e.movementY * this.scaleHeight / zoomClearance;
+                        this.drawTrimmingWidth -= e.movementY * this.scaleHeight / zoomClearance;
                     }
                     if (e.movementX != 0 && e.movementY != 0) {
                         this.dx += e.movementX * this.scaleWidth / zoomClearance;
-                        this.dx -= e.movementY * this.scaleHeight / zoomClearance;
+                        this.dx += e.movementY * this.scaleHeight / zoomClearance;
                         this.drawTrimmingWidth -= e.movementX * this.scaleWidth / zoomClearance;
-                        this.drawTrimmingWidth += e.movementY * this.scaleHeight / zoomClearance;
+                        this.drawTrimmingWidth -= e.movementY * this.scaleHeight / zoomClearance;
                     }
                     this.drawTrimmingHeight = this.drawTrimmingWidth;
 
