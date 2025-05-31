25de39cb6b5e105fa26b338a985bd110d29f920a
Nfolio <139557367+Nfolio567@users.noreply.github.com>
2025-05-31 18:11:03 +0900

    safari

--- src/simprim.ts
+++ src/simprim.ts
@@ -119,7 +119,7 @@ class SimPrim {
         });
 
         this.inputCvs?.addEventListener("mousemove", (e) => {
-            if (this.animationFrameID !== undefined) cancelAnimationFrame(this.animationFrameID);
+            //if (this.animationFrameID !== undefined) cancelAnimationFrame(this.animationFrameID);
             if (this.defaultCursor && this.inputCvs) this.inputCvs.style.cursor = "default"; // Reset mouse to default
 
             if (this.dx !== undefined) this.cx = this.dx / this.scaleWidth + this.drawTrimmingWidth / this.scaleWidth / 2; // Calculate center coordinate
