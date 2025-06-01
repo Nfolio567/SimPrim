af5de236ca3afa70e5a46f2d75633e32af9cba66
Nfolio <139557367+Nfolio567@users.noreply.github.com>
2025-06-01 19:18:50 +0900

    safari

--- src/simprim.ts
+++ src/simprim.ts
@@ -250,6 +250,7 @@ class SimPrim {
                     if (e.movementX !== 0 && e.movementY !== 0) {
                         this.drawTrimmingWidth += (e.movementX * this.scaleWidth) / zoomClearance;
                         this.drawTrimmingWidth += (e.movementY * this.scaleHeight) / zoomClearance;
+                        console.log("###########");
                     }
                     this.drawTrimmingHeight = this.drawTrimmingWidth;
                     // Out-of-bounds check
